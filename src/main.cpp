24419c080442954e10d874ad33a36d5b382fa7de
sajith <sajith@zmessenger.com>
2020-04-04 17:14:56 +0530

    Added a cpp file for drawing a shapes

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,58 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <iostream>
+#include<opencv2/core.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/highgui.hpp>
+
+#define w 400
 
 using namespace cv;
-using namespace std;
 
-int main( int argc, char** argv )
-{
-    // need to be changed based on the running location or please add full path 
-    String imageName( "./images/cute-dog.jpg" ); 
-    if( argc > 1)
-    {
-        imageName = argv[1];
-    }
-    Mat image;
-    image = imread( samples::findFile( imageName ), IMREAD_COLOR ); // Read the file
-    if( image.empty() )                      // Check for invalid input
-    {
-        cout <<  "Could not open or find the image" << std::endl ;
-        return -1;
-    }
-    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
-    imshow( "Display window", image );                // Show our image inside it.
-    waitKey(0); // Wait for a keystroke in the window
+void MyEllipse(Mat img, double angle);
+
+void MyFilledCircle(Mat img, Point center);
+
+int main() {
+    char atom_window[] = "Drawing 1: Atom";
+
+    Mat atom_image = Mat::zeros(w, w, CV_8UC3);
+
+
+    MyEllipse(atom_image, 90.0);
+    MyEllipse(atom_image, 0);
+    MyEllipse(atom_image, 45);
+    MyEllipse(atom_image, -45);
+
+    MyFilledCircle(atom_image, Point(w / 2, w / 2));
+
+    cv::imshow(atom_window, atom_image);
+    cv::moveWindow(atom_window, 0, 200);
+    waitKey(0);
+
     return 0;
+
+}
+
+void MyEllipse(Mat img, double angle) {
+    int thickness = 2;
+    int lineType = 8;
+
+    cv::ellipse(img,
+                cv::Point(w / 2, w / 2),
+                cv::Size(w / 4, w / 4),
+                angle,
+                0,
+                360,
+                cv::Scalar(255, 0, 0),
+                thickness,
+                lineType);
+}
+
+
+void MyFilledCircle(Mat img, Point center) {
+    cv::circle(img,
+               center,
+               w / 32,
+               Scalar(0, 0, 255),
+               FILLED,
+               LINE_8);
+
 }
